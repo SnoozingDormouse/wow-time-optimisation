84c5a95c16b050e429e22d9610497a12d436b2b8
Dorothy Hawley <snoozing.dormouse@gmail.com>
2020-01-28 21:19:11 +0000

    removed default weatherforecast items

---
+++ WoWTimeOptimisation/Controllers/BattlePetController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WoWTimeOptimisation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BattlePetController : ControllerBase
+    {
+
+        private readonly ILogger<BattlePetController> _logger;
+
+        public BattlePetController(ILogger<BattlePetController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<BattlePet> Get()
+        {
+        }
+    }
+}
