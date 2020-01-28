192e2264eb833717c4b60f1b20acd9c657444417
Dorothy Hawley <snoozing.dormouse@gmail.com>
2020-01-28 21:55:03 +0000

    added swagger

--- WoWTimeOptimisationAPI/Startup.cs
+++ WoWTimeOptimisationAPI/Startup.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+//using WoWToolsCore.Services;
+//using WoWToolsCore.Services.BlizzardAPIServices;
+//using WoWToolsCore.Services.InternalServices;
 
 namespace WoWTimeOptimisation
 {
@@ -25,20 +30,53 @@ namespace WoWTimeOptimisation
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            //services.AddHttpClient();
+            //services.AddSingleton<IBlizzardAPIService>(s => new BlizzardAPIService(s.GetService<IHttpClientFactory>(), Configuration["Battlenet:ClientId"], Configuration["Battlenet:ClientSecret"]));
+            //services.AddSingleton<ICharacterInfoCache, CharacterQuestCache>();
+            //services.AddSingleton<ICharacterService, CharacterService>();
+            //services.AddSingleton<IQuestChainService, QuestChainService>();
+
+ 			services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "WoW Time Optimisation API",
+                    Description = "API for WoW Time Optimisation",
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+			app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WowToolsCore API 0.1.0"));
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+			app.UseStaticFiles();
 
             app.UseAuthorization();
 
