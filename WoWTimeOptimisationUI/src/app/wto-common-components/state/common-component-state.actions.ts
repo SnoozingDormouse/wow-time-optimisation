cd8c178874c4ef7de3d52fd9a267e7f68960e8cf
Dorothy Hawley <snoozing.dormouse@gmail.com>
2020-01-29 22:04:21 +0000

    initialized actions array before use.

--- WoWTimeOptimisationUI/src/app/wto-common-components/state/common-component-state.actions.ts
+++ WoWTimeOptimisationUI/src/app/wto-common-components/state/common-component-state.actions.ts
@@ -1,6 +1,5 @@
 import { createAction } from '@ngrx/store';
 import { commonComponentActionKey } from './i-common-component-state.actionkey';
-import { OnInit } from '@angular/core';
 
 // actions are 'delegates' that set / change the values in the store (or slice of store)
 // we use createAction to create an action in ngrx
@@ -9,15 +8,15 @@ import { OnInit } from '@angular/core';
 // we append the component undergoing the action to create a unique verb-noun combination
 // that can be used by the reducer to act on the actions.
 
-export class CreateActions implements OnInit  {
+export class CreateActions  {
 
-    public actions: object;
+    public actions: object = {};
 
-    constructor(public componentName: string) {}
-
-    ngOnInit(): void {
-
+    constructor(public componentName: string) {
         const key = commonComponentActionKey.toggleAnimation + this.componentName;
         this.actions[key] = createAction(key);
+
+        console.log('action: ');
+        console.log(this.actions[key]);
     }
 }
