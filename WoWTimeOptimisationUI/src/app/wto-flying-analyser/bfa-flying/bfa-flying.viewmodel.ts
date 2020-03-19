8a2e42a760ce35c27457fb78b5e9487b06d22a84
Dorothy Hawley <snoozing.dormouse@gmail.com>
2020-03-19 10:03:44 +0000

    Feature/display table for body (#7)
    * shows mock data as BFAFlying table
    
    * variable number of characters displaying in materials table

---
+++ WoWTimeOptimisationUI/src/app/wto-flying-analyser/bfa-flying/bfa-flying.viewmodel.ts
@@ -0,0 +1,60 @@
+import { IStep, ICharacterStepStatus } from './state/i-bfa-flying-state';
+import { Injectable } from '@angular/core';
+
+export interface IBFAFlyingViewModel {
+    headers: string[];
+    datasource: Array<IBFAFlyingViewNode>
+}
+
+interface IBFAFlyingViewNode {
+    criteria: ICriteria;
+    charscompleted: Array<ICharCompleted>;
+}
+
+interface ICriteria {
+    criteriaId: number;
+    name: string;
+}
+
+interface ICharCompleted {
+    criteriaId: number;
+    iscomplete: boolean;
+    amount: number;
+}
+
+@Injectable()
+export class BFAFlyingViewModel {
+    public populateViewModel(steps: IStep[], characterSteps: ICharacterStepStatus[]): IBFAFlyingViewModel {
+        const viewModel: Array<IBFAFlyingViewNode> = [];
+        const characters: Array<string> = [];
+
+        steps.map(x => {
+            viewModel.push(
+            {
+                criteria: { criteriaId: x.criteriaId, name: x.name },
+                charscompleted: [],
+            })});
+
+        let index = 0;
+
+        characterSteps.forEach(namedCharacter =>
+            {
+                characters.push(namedCharacter.characterName);
+
+                namedCharacter.characterSteps
+                .map(cs => {
+                    viewModel
+                    .filter( v => v.criteria.criteriaId === cs.criteriaId)
+                    .map(e => e.charscompleted[index] =
+                        { criteriaId: cs.criteriaId, iscomplete: cs.isComplete, amount: cs.amount });
+                });
+                index++;
+            }
+        );
+
+        return {
+            headers: characters,
+            datasource: viewModel
+        }
+    }
+}
--- WoWTimeOptimisationUI/src/app/wto-flying-analyser/bfa-flying/state/bfa-flying.state.index.ts
+++ WoWTimeOptimisationUI/src/app/wto-flying-analyser/bfa-flying/state/bfa-flying.state.index.ts
@@ -3,5 +3,6 @@ export * from './bfa-flying.state.actions';
 export * from './bfa-flying.state.reducers';
 export * from './bfa-flying.state.types';
 export * from './bfa-flying.state.selectors';
+export * from './bfa-flying.state.initializers';
 
 export const featureKey = 'bfaFlyingState';
